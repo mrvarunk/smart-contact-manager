a9b235cf31c54597866b630385900e156ac57a40
Varun K <156565349+mrvarunk@users.noreply.github.com>
2026-03-18 13:10:32 +0530

    Add main application entry point for SCM

---
+++ src/main/java/com/project/scm/MainApplication.java
@@ -0,0 +1,7 @@
+// Entry point of the Smart Contact Manager application
+
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Smart Contact Manager Started");
+    }
+}
